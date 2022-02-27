b7e974bc8382355fa808a301d48d66faa18d6640
Jukis252 <74786908+Jukis252@users.noreply.github.com>
2022-02-27 18:16:36 +0200

    Coherent homework5_task1

---
+++ CoherentHW5/CoherentHW5_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using CoherentHW5_Task1.Entities;
+
+namespace CoherentHW5_Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            var sparseMatrix = new SparseMatrix(5, 5);
+            sparseMatrix[0, 0] = 1;
+            sparseMatrix[0, 1] = 5;
+            sparseMatrix[1, 1] = 3;
+            sparseMatrix[1, 2] = 4;
+            sparseMatrix[2, 2] = 5;
+            sparseMatrix[2, 3] = 6;
+            sparseMatrix[3, 3] = 7;
+            sparseMatrix[3, 4] = 5;
+            sparseMatrix[4, 4] = 9;
+
+            Console.WriteLine(sparseMatrix);
+            foreach (var element in sparseMatrix)
+            {
+                Console.WriteLine(element);
+            }
+
+            foreach (var element in sparseMatrix.NoZeroOutput())
+            {
+                Console.WriteLine($"Value {element.Item3} in column " + $"{element.Item1 + 1} and row {element.Item2 + 1}");
+            }
+
+            Console.WriteLine();
+            int occurrencesOfCount = sparseMatrix.Count(5);
+            Console.WriteLine($"Occurrences of number 5 in matrix is: {occurrencesOfCount}");
+        }
+    }
+}

66a1414be8e5c43d91c88f3661c1d9af39ef4492
Jukis252 <74786908+Jukis252@users.noreply.github.com>
2022-02-28 02:24:57 +0200

    Initial commit of HW5_Task3

---
+++ CoherentHW5/CoherentHW5_Task3/Entities/Management.cs
@@ -0,0 +1,65 @@
+﻿namespace CoherentHW5_Task3.Entities;
+
+internal class Management
+{
+    private readonly List<EmployeeVacations> _employeeVacations;
+
+    public Management()
+    {
+        _employeeVacations = new List<EmployeeVacations>();
+    }
+
+    public void AddVacation(EmployeeVacations vacation)
+    {
+        if (vacation.IsEmployeeAStudent)
+            _employeeVacations.Add(vacation);
+        else if (!_employeeVacations.Any(
+                     vacationRecord => vacationRecord.NameOfEmployee.Equals(vacation.NameOfEmployee)))
+            _employeeVacations.Add(vacation);
+        else
+            throw new Exception("Vacation limit reached.");
+    }
+
+    public bool VacationCrossingDates()
+    {
+        return _employeeVacations
+            .Any(item => _employeeVacations
+                .Where(otherItem => otherItem != item)
+                .Any(otherItem => otherItem.NameOfEmployee.Equals(item.NameOfEmployee)
+                                  && otherItem.LastVacationDay >= item.FirstVacationDay
+                                  && otherItem.FirstVacationDay <= item.LastVacationDay));
+    }
+
+    public IEnumerable<(string, double)> AverageVacationLengthOfEmployees()
+    {
+        var employeeVacationAverage = _employeeVacations.GroupBy(employee => employee.NameOfEmployee,
+                employeeVacation => employeeVacation.EmployeesAllVacationTime())
+            .Select(employeeVacationAvg => (employeeVacationAvg.Key, employeeVacationAvg.Average()));
+
+        foreach (var employee in employeeVacationAverage) yield return (employee.Key, employee.Item2);
+    }
+
+    public double AverageVacationLengthOfOrganization()
+    {
+        return _employeeVacations.Average(employee => employee.EmployeesAllVacationTime());
+    }
+
+    public IEnumerable<DateTime> DateNoEmployeeOnVacation()
+    {
+        var startOfYearDate = new DateTime(2021, 01, 01);
+        var endOfYearDate = new DateTime(2021, 12, 31);
+
+        var allDatesOfYear = Enumerable.Range(0, endOfYearDate.Subtract(startOfYearDate).Days + 1)
+            .Select(day => startOfYearDate.AddDays(day));
+
+        return allDatesOfYear.Except(
+            _employeeVacations.SelectMany(employee => employee.GetAllVacationDatesOfEmployee()));
+    }
+
+    public IEnumerable<(int, int)> MonthlyDistributionOfEmployeeVacation()
+    {
+        foreach (var month in Enumerable.Range(1, 12).ToList())
+            yield return (month, _employeeVacations.Count(vacation => vacation.FirstVacationDay.Month <= month
+                                                                      && vacation.LastVacationDay.Month >= month));
+    }
+}
---
+++ CoherentHW5/CoherentHW5_Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using CoherentHW5_Task3.Entities;
+
+namespace CoherentHW5_Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var firstEmployee = new EmployeeVacations("John", new DateTime(2021, 1, 1), new DateTime(2021, 1, 30));
+            var secondEmployee = new EmployeeVacations("Jeff", new DateTime(2021, 2, 1), new DateTime(2021, 6, 1));
+            var thirdEmployee = new EmployeeVacations("Jim", new DateTime(2021, 3, 5), new DateTime(2021, 3, 6));
+            var fourthEmployee = new EmployeeVacations("Janice", new DateTime(2021, 4, 11), new DateTime(2021, 4, 18));
+            var fifthEmployee = new EmployeeVacations("Jerry", new DateTime(2021, 5, 29), new DateTime(2021, 6, 1));
+
+            firstEmployee.IsEmployeeAStudent = true;
+            secondEmployee.IsEmployeeAStudent = true;
+            thirdEmployee.IsEmployeeAStudent = true;
+            fourthEmployee.IsEmployeeAStudent = false;
+            fifthEmployee.IsEmployeeAStudent = false;
+
+            var managementVacation = new Management();
+
+            managementVacation.AddVacation(firstEmployee);
+            managementVacation.AddVacation(secondEmployee);
+            managementVacation.AddVacation(thirdEmployee);
+            managementVacation.AddVacation(fourthEmployee);
+            managementVacation.AddVacation(fifthEmployee);
+
+            double result = managementVacation.AverageVacationLengthOfOrganization();
+            Console.WriteLine("Average vacation length in organization: " + result);
+
+
+            foreach(var employee in managementVacation.AverageVacationLengthOfEmployees())
+			{
+				Console.WriteLine($"{employee.Item1}: {employee.Item2}");
+			}
+
+		    foreach(var month in managementVacation.MonthlyDistributionOfEmployeeVacation())
+		    {
+			    Console.WriteLine($"Month of the year(number): {month.Item1}, " +
+			    $"Employee count that month(number): {month.Item2}");
+		    }
+            Console.WriteLine("Free vacation dates:");
+			foreach(var day in managementVacation.DateNoEmployeeOnVacation())
+			{
+				Console.WriteLine(day.Date);
+			}
+
+            Console.WriteLine(managementVacation.VacationCrossingDates());
+        }
+    }
+}
+
